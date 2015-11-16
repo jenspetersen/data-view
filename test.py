9f613f4f6968e179a353e8efa3e1325536404071
Jens Petersen <jens.petersen@dkfz-heidelberg.de>
2015-11-16 18:54:36 +0100

    Implement interaction widget

--- test.py
+++ test.py
@@ -75,10 +75,9 @@ class SliceView(QtGui.QWidget):
         self.__imshowAccessor = None
 
         # layout
-        layout = QtGui.QVBoxLayout()
-        layout.addWidget(self.__canvas)
-        layout.setContentsMargins(0, 0, 0, 0)
-        self.setLayout(layout)
+        self.layout = QtGui.QVBoxLayout(self)
+        self.layout.addWidget(self.__canvas)
+        self.layout.setContentsMargins(0, 0, 0, 0)
         self.setDisabled(True)
 
         # prepare figure
@@ -98,6 +97,8 @@ class SliceView(QtGui.QWidget):
         else:
             self.__imshowAccessor.set_data(data)
             self.__canvas.draw()
+        self.setMinimumSize(max(data.shape[0], 10),
+                            max(data.shape[1], 10))
 
 
 class VolumeView(QtGui.QWidget):
@@ -105,6 +106,7 @@ class VolumeView(QtGui.QWidget):
     def __init__(self, parent=None, data=None, axis=0):
 
         super(VolumeView, self).__init__(parent)
+        self.parent = parent
         self.data = data
         self.axis = axis
 
@@ -112,10 +114,9 @@ class VolumeView(QtGui.QWidget):
         self.sliceView = SliceView(self)
 
         # layout
-        layout = QtGui.QVBoxLayout()
-        layout.addWidget(self.sliceView)
-        layout.setContentsMargins(0, 0, 0, 0)
-        self.setLayout(layout)
+        self.layout = QtGui.QVBoxLayout(self)
+        self.layout.addWidget(self.sliceView)
+        self.layout.setContentsMargins(0, 0, 0, 0)
 
         if self.data is not None:
 
@@ -124,15 +125,18 @@ class VolumeView(QtGui.QWidget):
             self.setSlice(self.currentSlice)
 
     def wheelEvent(self, event):
+
         self.currentSlice = (self.currentSlice + int(event.delta() / 120))\
             % self.numberOfSlices
         print self.currentSlice
         self.setSlice(self.currentSlice)
 
     def setData(self, data):
+
         self.__init__(self.parent, data, self.axis)
 
     def setSlice(self, sliceNumber):
+
         if self.axis == 0:
             self.sliceView.setData(self.data[sliceNumber, :, :])
         elif self.axis == 1:
@@ -143,6 +147,7 @@ class VolumeView(QtGui.QWidget):
             raise IndexError("Index out of range.")
 
     def setAxis(self, axis):
+
         if not isinstance(axis, int):
             raise ValueError("Index must be integer.")
         if 0 <= axis <= 2:
@@ -152,6 +157,149 @@ class VolumeView(QtGui.QWidget):
             self.setSlice(self.currentSlice)
         else:
             raise IndexError("Index out of range.")
+
+
+class InteractionWidget(QtGui.QWidget):
+
+    def __init__(self, parent=None):
+
+        super(InteractionWidget, self).__init__(parent)
+
+        self.buttons = []
+        self.interactors = []
+
+        # outer layout
+        self.layout = QtGui.QVBoxLayout(self)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.layout.setSpacing(0)
+        self.setSizePolicy(QtGui.QSizePolicy.Minimum,
+                           QtGui.QSizePolicy.Fixed)
+
+        # label container
+        self.labelFrame = QtGui.QFrame(self)
+        self.labelFrame.setFrameShape(QtGui.QFrame.NoFrame)
+        self.labelLayout = QtGui.QHBoxLayout(self.labelFrame)
+        self.labelLayout.setContentsMargins(5, 5, 5, 0)
+
+        # labels
+        self.leftLabel = QtGui.QLabel(self.labelFrame)
+        self.rightLabel = QtGui.QLabel(self.labelFrame)
+        self.rightLabel.setAlignment(QtCore.Qt.AlignRight |
+                                     QtCore.Qt.AlignTrailing)
+
+        self.labelLayout.addWidget(self.leftLabel)
+        labelSpacer = QtGui.QSpacerItem(10, 20,
+                                        QtGui.QSizePolicy.Expanding,
+                                        QtGui.QSizePolicy.Minimum)
+        self.labelLayout.addItem(labelSpacer)
+        self.labelLayout.addWidget(self.rightLabel)
+
+        # interaction outer container
+        self.interactionFrame = QtGui.QFrame(self)
+        self.interactionFrame.setFrameShape(QtGui.QFrame.NoFrame)
+        self.interactionLayout = QtGui.QHBoxLayout(self.interactionFrame)
+        self.interactionLayout.setContentsMargins(0, 0, 0, 0)
+
+        # interaction button container
+        self.buttonFrame = QtGui.QFrame(self.interactionFrame)
+        self.buttonFrame.setFrameShape(QtGui.QFrame.NoFrame)
+        self.buttonLayout = QtGui.QHBoxLayout(self.buttonFrame)
+        self.buttonLayout.setContentsMargins(5, 5, 5, 5)
+
+        # interaction inner container
+        self.interactionInnerFrame = QtGui.QFrame(self.interactionFrame)
+        self.interactionInnerFrame.setFrameShape(QtGui.QFrame.NoFrame)
+        self.interactionInnerLayout = QtGui.QHBoxLayout(
+            self.interactionInnerFrame)
+        self.interactionInnerLayout.setContentsMargins(5, 5, 5, 0)
+
+        self.interactionLayout.addWidget(self.interactionInnerFrame)
+        interactionSpacer = QtGui.QSpacerItem(10, 20,
+                                              QtGui.QSizePolicy.Expanding,
+                                              QtGui.QSizePolicy.Minimum)
+        self.interactionLayout.addItem(interactionSpacer)
+        self.interactionLayout.addWidget(self.buttonFrame)
+
+        # outer layout
+        self.layout.addWidget(self.labelFrame)
+        self.layout.addWidget(self.interactionFrame)
+
+    def setLeftLabel(self, labelText):
+
+        self.leftLabel.setText(labelText)
+
+    def setRightLabel(self, labelText):
+
+        self.rightLabel.setText(labelText)
+
+    def addButton(self, buttonText):
+
+        self.buttons.append(QtGui.QPushButton(self.buttonFrame))
+        self.buttons[-1].setText(buttonText)
+        self.buttonLayout.addWidget(self.buttons[-1])
+
+    def removeButton(self, buttonID):
+
+        self.buttonLayout.removeWidget(self.buttons[buttonID])
+        del self.buttons[buttonID]
+
+    def addLineEdit(self, lineEditText=''):
+
+        self.interactors.append(QtGui.QLineEdit(self.interactionInnerFrame))
+        self.interactors[-1].setText(lineEditText)
+        self.interactionInnerLayout.addWidget(self.interactors[-1])
+
+    def addCheckBox(self, checkBoxText):
+
+        self.interactors.append(QtGui.QCheckBox(self.interactionInnerFrame))
+        self.interactors[-1].setText(checkBoxText)
+        self.interactionInnerLayout.addWidget(self.interactors[-1])
+
+    def addComboBox(self, options):
+
+        self.interactors.append(QtGui.QComboBox(self.interactionInnerFrame))
+        for option in options:
+            self.interactors[-1].addItem(option)
+        self.interactors[-1].setCurrentIndex(0)
+        self.interactionInnerLayout.addWidget(self.interactors[-1])
+
+    def removeInteractor(self, interactorID):
+
+        self.interactionInnerLayout.removeWidget(
+            self.interactors[interactorID])
+        del self.interactors[interactorID]
+
+
+class VolumeViewInteraction(QtGui.QWidget):
+
+    def __init__(self, parent=None, data=None, axis=0, position="bottom"):
+
+        # check inputs
+        if position not in ["top", "bottom"]:
+            raise ValueError("Position must be either 'top' or 'bottom'")
+
+        super(VolumeViewInteraction, self).__init__(parent)
+
+        # initialize children
+        self.volumeView = VolumeView(self, data, axis)
+        self.interactionWidget = InteractionWidget(self)
+
+        # layout
+        self.layout = QtGui.QVBoxLayout(self)
+        if position == "top":
+            self.layout.addWidget(self.interactionWidget)
+            self.layout.addWidget(self.volumeView)
+        elif position == "bottom":
+            self.layout.addWidget(self.volumeView)
+            self.layout.addWidget(self.interactionWidget)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.layout.setSpacing(0)
+
+        self.interactionWidget.setRightLabel("0/0")
+
+    def setData(self, data):
+
+        self.volumeView.setData(data)
 
 
 # =============================================================================
@@ -167,7 +315,7 @@ if __name__ == "__main__":
 
     app = QtGui.QApplication(sys.argv)
 
-    main = VolumeView(data=np.random.rand(100, 100, 100))
+    main = VolumeViewInteraction(data=np.random.rand(100, 100, 100))
     main.show()
 
     sys.exit(app.exec_())
---
+++ testui.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'test.ui'
+#
+# Created: Mon Nov 16 11:52:34 2015
+#      by: PyQt4 UI code generator 4.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName(_fromUtf8("Form"))
+        Form.resize(452, 62)
+        self.frame = QtGui.QFrame(Form)
+        self.frame.setGeometry(QtCore.QRect(10, 10, 431, 43))
+        self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtGui.QFrame.Raised)
+        self.frame.setObjectName(_fromUtf8("frame"))
+        self.horizontalLayout = QtGui.QHBoxLayout(self.frame)
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.label = QtGui.QLabel(self.frame)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.horizontalLayout.addWidget(self.label)
+        spacerItem = QtGui.QSpacerItem(153, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        self.label_2 = QtGui.QLabel(self.frame)
+        self.label_2.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.horizontalLayout.addWidget(self.label_2)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(_translate("Form", "Form", None))
+        self.label.setText(_translate("Form", "Hallohallasdasdsad", None))
+        self.label_2.setText(_translate("Form", "asdasdasdasdasd", None))
+
