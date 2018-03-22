43cae13963be0013dfebb424b74cd696f25b0bc5
Jens Petersen <jens.petersen@dkfz.de>
2018-03-22 18:50:34 +0100

    desktop shortcut should be installable,not working

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup
 
+with open("README.md") as f:
+    readme = f.read()
+
+with open("LICENSE") as f:
+    license = f.read()
+
 setup(name='dataview',
       version='0.1',
       description='Package to quickly view data arrays',
+      long_description=readme,
+      url="https://github.com/jenspetersen/data-view",
       author='Jens Petersen',
       author_email='jens.petersen@dkfz.de',
-      license='MIT',
+      license=license,
       packages=['dataview'],
       install_requires=['numpy', 'matplotlib', 'qtpy'],
-      zip_safe=True)
+      entry_points={"gui_scripts": ["dataviewer=dataview.view:main"]},
+      data_files=[("share/applications", ["data/dataviewer.desktop"])])
